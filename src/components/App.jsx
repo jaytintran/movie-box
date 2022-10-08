f5746c218de5b6475728e309934d2f39034d4c19
Jay Tin <jaytintran@gmail.com>
2022-10-09 01:09:56 +0200

    fixing code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+// import { Route, Switch } from 'react-router-dom';
 
-const App = () => {
-  <div>
-    App
-  </div>;
-};
+function App() {
+  return (
+    <div>
+      <CssBaseline />
+      <h1>Hello world, Movie Box.</h1>
+      <p>AAAA</p>
+    </div>
+  );
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './components/App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import { BrowserRouter } from 'react-router-dom';
+// import App from './components/App.jsx';
+// import './index.css';
+import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
